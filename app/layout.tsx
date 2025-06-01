9bcd3ce17655b9ff476e462e207f89974f0320f2
shivendrarai <deritshivendra@gmail.com>
2025-06-01 16:20:14 +0530

    font and tailwindcss Configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,19 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono as GeistMono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = GeistMono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const inter = localFont({
+  src: "./fonts/InterVF.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 600 700 800 900",
 });
 
 export const metadata: Metadata = {
   title: "Devoverflow",
   description: "Devoverflow is a Q&A platform for developers",
+  icons: {
+    icon: "/images/site-logo.svg",
+  },
 };
 
 export default function RootLayout({
@@ -24,9 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} ${inter.variable} antialiased `}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <h1 className="text-3xl text-violet-400 font-black">Home</h1>;
+  return <h1 className="text-3xl text-light-500 bg-primary-100">Home</h1>;
 };
 
 export default Home;
